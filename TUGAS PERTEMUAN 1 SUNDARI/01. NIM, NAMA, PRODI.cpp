3349ac0aa4a94a1d7e6ae9850e5acd71f48c1af7
Sundari Rosdiana <rosdianasundari@gmail.com>
2023-10-21 15:53:43 +0700

    tugas praktikum sabtu c

---
+++ TUGAS PERTEMUAN 1 SUNDARI/01. NIM, NAMA, PRODI.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(){
+    printf("Program Menampilkan Nama, NIM dan Jurusan"); 
+    printf("\n");
+    printf("NAMA: Sundari Rosdiana");  
+	printf("\n");          
+    printf("NIM: 234051");
+	printf("\n");                              
+    printf("JURUSAN: Teknik Informatika");            
+    return 0;                                   
+}
