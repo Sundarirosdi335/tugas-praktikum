07830422167eeac6efa5a5c6663d33e42bc6405d
Sundarirosdi335 <147240344+Sundarirosdi335@users.noreply.github.com>
2023-11-25 15:28:07 +0700

    TUGAS PRAKTIKUM SABTU

---
+++ PERTEMUAN 6 SUNDARI ROSDIANA/PERTEMUAN 6 NO 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf ("## Program Bahasa C Deret Angka ## \n");
+	printf ("================================== \n\n");
+	
+	{
+		for (int i = 1; i <= 10; i++)
+		printf("%d ",i *10);
+	}
+	return 0;
+}
---
+++ PERTEMUAN 6 SUNDARI ROSDIANA/PERTEMUAN 6 NO 5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	printf ("## Program Bahasa C Menghitung & Mencetak Jumlah Uang  ## \n");
+	printf ("================================== \n\n");
+
+		double saldo = 1000000;
+		double bunga = 0.02;
+		for (int bulan = 1; bulan <= 10; ++bulan){
+		}
+		printf("Jumlah uang setelah 10 bulan: Rp. %.2f\n", saldo);
+		return 0;
+}
+
